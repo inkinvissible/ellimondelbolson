7e225acc7097ce638f21b30b0bf862d7b9f5ad7a
candepina <candepina475@gmail.com>
2025-02-04 18:01:36 -0300

    arreglos en navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,23 +23,26 @@ $(function() {
     });
     
     
-    //===== Section Menu Active
-
-    var scrollLink = $('.page-scroll');
-    // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
-
-        scrollLink.each(function () {
-
-            var sectionOffset = $(this.hash).offset().top - 73;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
+    $(document).ready(function () {
+        var scrollLink = $('.page-scroll');
+    
+        $(window).scroll(function () {
+            var scrollbarLocation = $(this).scrollTop();
+    
+            scrollLink.each(function () {
+                var target = $(this.hash);
+                if (target.length) {  // Verifica si el hash tiene un elemento válido
+                    var sectionOffset = target.offset().top - 73;
+    
+                    if (scrollbarLocation >= sectionOffset) {
+                        $(this).parent().addClass('active');
+                        $(this).parent().siblings().removeClass('active');
+                    }
+                }
+            });
         });
     });
+    
     
     //===== close navbar-collapse when a  clicked
 
